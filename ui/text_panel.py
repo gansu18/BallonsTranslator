648d779d97533473401b015ff1121e69e83f80e0
dmMaze <beneathlimbo@gmail.com>
2025-03-04 14:36:50 +0800

    add apply opption for color picker #744

--- ui/text_panel.py
+++ ui/text_panel.py
@@ -283,6 +283,7 @@ class FontFormatPanel(Widget):
         self.colorPicker.setToolTip(self.tr("Change font color"))
         self.colorPicker.changingColor.connect(self.changingColor)
         self.colorPicker.colorChanged.connect(self.onColorLabelChanged)
+        self.colorPicker.apply_color.connect(self.on_apply_color)
 
         self.alignBtnGroup = AlignmentBtnGroup(self)
         self.alignBtnGroup.param_changed.connect(self.on_param_changed)
@@ -311,6 +312,7 @@ class FontFormatPanel(Widget):
         self.strokeColorPicker.setToolTip(self.tr("Change stroke color"))
         self.strokeColorPicker.changingColor.connect(self.changingColor)
         self.strokeColorPicker.colorChanged.connect(self.onColorLabelChanged)
+        self.strokeColorPicker.apply_color.connect(self.on_apply_color)
 
         stroke_hlayout = QHBoxLayout()
         stroke_hlayout.addWidget(self.fontStrokeLabel)
@@ -352,12 +354,17 @@ class FontFormatPanel(Widget):
         color_label = self.textadvancedfmt_panel.shadow_group.color_label
         color_label.changingColor.connect(self.changingColor)
         color_label.colorChanged.connect(self.onColorLabelChanged)
+        color_label.apply_color.connect(self.on_apply_color)
+
         color_label = self.textadvancedfmt_panel.gradient_group.start_picker
         color_label.changingColor.connect(self.changingColor)
         color_label.colorChanged.connect(self.onColorLabelChanged)
+        color_label.apply_color.connect(self.on_apply_color)
+        
         color_label = self.textadvancedfmt_panel.gradient_group.end_picker
         color_label.changingColor.connect(self.changingColor)
         color_label.colorChanged.connect(self.onColorLabelChanged)
+        color_label.apply_color.connect(self.on_apply_color)
         
         self.foldTextBtn = CheckableLabel(self.tr("Unfold"), self.tr("Fold"), False)
         self.sourceBtn = TextCheckerLabel(self.tr("Source"))
@@ -446,6 +453,9 @@ class FontFormatPanel(Widget):
             rgb = sender.rgb()
             self.on_param_changed(sender.param_name, rgb)
 
+    def on_apply_color(self, param_name, rgb):
+        self.on_param_changed(param_name, rgb)
+
     def onLineSpacingCtrlChanged(self, delta: int):
         if C.active_format.line_spacing_type == LineSpacingType.Distance:
             mul = 0.1
@@ -520,10 +530,7 @@ class FontFormatPanel(Widget):
                     self.textblk_item.fontformat = copy.deepcopy(C.active_format)
                 self.textblk_item = None
                 self.set_active_format(self.global_format, multi_select)
-                if multi_select:
-                    self.textstyle_panel.setTitle('Group')
-                else:
-                    self.set_globalfmt_title()
+                self.set_globalfmt_title()
             
         else:
             if not self.restoring_textblk:
